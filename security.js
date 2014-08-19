ae7393dcb8f128b9d6e1bb51ea845443ae84dc89
Gunnar Almås <gunnar.almaas@brunvoll.no>
2014-08-19 15:19:36 +0200

    Videre arbeid

--- security.js
+++ security.js
@@ -1,16 +1,18 @@
 angular.module('almgun.dom-sec' ,[]).
     provider('authentication', function () {
         // ...
-        $.get = {
-            logIn : function () {
-                console.log('this is the login-method');
+        this.$get = function () {
+            return {
+                logIn : function () {
+                    console.log('this is the login-method');
 
-            },
-            logOut: function () {
-                console.log('this is the logout-method');
-            },
-            isLoggedIn: function () {
-                console.log('this is the isLoggedIn-method');
+                },
+                logOut: function () {
+                    console.log('this is the logout-method');
+                },
+                isLoggedIn: function () {
+                    console.log('this is the isLoggedIn-method');
+                }
             }
         }
     });
