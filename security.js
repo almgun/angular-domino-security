2649e1a30415682407c2172462e34879126b20b4
Gunnar Almås <gunnar.almaas@brunvoll.no>
2014-08-23 12:48:18 +0200

    lagt til settings.js

--- security.js
+++ security.js
@@ -1,7 +1,7 @@
-angular.module('almgun.dom-sec', []).
+angular.module('ga.domino-utils', []).
     provider('authentication', function () {
         //Result from last call to logIn()
-       var  _status = {
+        var _status = {
             isLoggedIn: false,
             isConnected: false,
             user: {},
@@ -11,63 +11,58 @@ angular.module('almgun.dom-sec', []).
                 this.user = {};
             }
         };
-        var _host = "http://xxxdomino9/";
+        var _hostName = "http://xxxdomino9/";
         var _userInfoPath = 'dev/orders.nsf/api.xsp/userinfo';
         var _confirmLoginPath = 'dev/orders.nsf/login?OpenPage';
-
         var _logInPath = 'names.nsf?Login';
         var _logOutPath = 'names.nsf?Logout';
-
         this.hostName = function (host) {
-            if(host){
-                _host = host;
+            if (host) {
+                _hostName = host;
                 return this;
             }
-            else{
-                return _host;
+            else {
+                return _hostName;
             }
         };
-
         this.userInfoPath = function (path) {
-            if(path){
+            if (path) {
                 _userInfoPath = path;
                 return this;
             }
-            else{
+            else {
                 return _userInfoPath;
             }
         };
         this.confirmLoginPath = function (path) {
-            if(path){
+            if (path) {
                 _confirmLoginPath = path;
                 return this;
             }
-            else{
+            else {
                 return _confirmLoginPath;
             }
         };
-
-        // ...
         this.$get = function ($http, $q) {
             return {
                 logIn: function (user, pw) {
                     var conf = {
                         method: 'POST',     //Angular
                         type: 'POST',       //jQuery
-                        url: _host+_logInPath,
+                        url: _hostName + _logInPath,
                         data: {username: user || 'Web User', password: pw || 'almgun'},
                         xhrFields: {
                             withCredentials: true //jQuery
                         },
                         withCredentials: true  //Angular
 
-                    }
-                    console.log('this is the login-method ' );
+                    };
+                    console.log('this is the login-method ');
                     var pr = jQuery.ajax(conf);  //fungerer bare med jQuery vet ikke hvorfor
                     var deferred = $q.defer(); //Returnerer denne
 
-                    $q.when(pr).then(function (resolved) {
-                        console.debug('login OK (method POST)' );
+                    $q.when(pr).then(function () {
+                        console.debug('login OK (method POST)');
                         return this.getUser();
                     }.bind(this)).then(function (resolved) {
                         deferred.resolve(resolved);
@@ -76,18 +71,18 @@ angular.module('almgun.dom-sec', []).
                         _status.reset();
                         console.error('Error in promise chain ' + rejected);
                         deferred.reject('Error in login-function');
-                    })
+                    });
 
                     return deferred.promise;
 
                 },
                 logOut: function () {
-                    var str = _host+_logOutPath;
+                    var str = _hostName + _logOutPath;
                     var pr = $http.get(str);
                     pr.then(function () {
                         _status.reset();
                         console.debug('log-out OK');
-                    })
+                    });
                     return pr;
                 },
                 isLoggedIn: function () {
@@ -95,32 +90,31 @@ angular.module('almgun.dom-sec', []).
                     var deferred = $q.defer();
                     var conf = {
                         method: 'OPTIONS',
-                        cache:false,
-                        url: _host+_confirmLoginPath
+                        cache: false,
+                        url: _hostName + _confirmLoginPath
                     };
                     var prom = $http(conf);
                     prom.then(function (res) {
-                        if(res.data)
-                        {
-                           deferred.reject('NOT-LOGGED-IN');
+                        if (res.data) {
+                            deferred.reject('NOT-LOGGED-IN');
                         }
                         {
                             deferred.resolve('LOGGED-IN');
                         }
-                    }, function (rej) {
+                    }, function () {
                         deferred.reject('NOT-CONNECTED');
-                    })
+                    });
                     return deferred.promise;
                 },
                 getUser: function () {
                     var deferred = $q.defer();
                     var conf = {
                         method: 'GET',
-                        url: _host+_userInfoPath
+                        url: _hostName + _userInfoPath
                     };
                     var prom = $http(conf);
                     //Must use succsess to get header obj
-                    prom.success(function (userObj, status, header, config) {
+                    prom.success(function (userObj, status, header) {
                         var respHeader = header();
                         var html = respHeader['content-type'].match(/text\/html/);
                         if (!html) {
@@ -143,7 +137,37 @@ angular.module('almgun.dom-sec', []).
 
                     });
                     return deferred.promise;
+                },
+                localGetProviderConfig: function () {
+                    return Object.freeze({
+                            hostName: _hostName,
+                            userInfoPath: _userInfoPath,
+                            confirmLoginPath: _confirmLoginPath
+                        }
+                    )
+
+
+                },
+                localGetUser: function () {
+                    return Object.freeze(_status.user)
                 }
             }
         }
+    }
+).provider('authSaml', function () {
+        return{
+            $get: function () {
+
+            }
+        }
+
+}
+).provider('authBasic', function () {
+        return{
+            $get: function () {
+
+            }
+        }
     });
+
+
---
+++ settings.js
@@ -0,0 +1,17 @@
+/**
+ * Created by almgun on 23.08.2014.
+ */
+angular.module('ga.domino-utils').provider('appSettings', function () {
+    return{
+        $get: function () {
+
+        }
+    }
+}
+).provider('appConfig', function () {
+        return{
+            $get: function () {
+
+            }
+        }
+    })
